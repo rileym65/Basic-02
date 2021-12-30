1a0a39e49683e451ded88725d2fbdb1e6ca7695e
Mike Riley <mikehriley@hotmail.com>
2021-12-30 15:23:19 -0800

    Fixed bug where fread and fwrite would not work in 32-bit mode

--- cfread.c
+++ cfread.c
@@ -28,12 +28,12 @@ char* cfread(char* line) {
   line = cexpr(line, 0);
   if (use32Bits) {
     Asm("          inc   r7                      ; Retrieve address");
-    Asm("          inc   r7");
-    Asm("          inc   r7");
     Asm("          lda   r7");
     Asm("          stxd                          ; save on stack for now");
     Asm("          ldn   r7");
     Asm("          stxd");
+    Asm("          inc   r7");
+    Asm("          inc   r7");
     }
   else {
     Asm("          inc   r7                      ; Retrieve address");
@@ -51,12 +51,12 @@ char* cfread(char* line) {
   line = cexpr(line, 0);
   if (use32Bits) {
     Asm("          inc   r7                      ; Retrieve address");
-    Asm("          inc   r7");
-    Asm("          inc   r7");
     Asm("          lda   r7");
     Asm("          plo   rc                      ; place into count register");
     Asm("          ldn   r7");
     Asm("          phi   rc");
+    Asm("          inc   r7");
+    Asm("          inc   r7");
     }
   else {
     Asm("          inc   r7                      ; Retrieve address");
